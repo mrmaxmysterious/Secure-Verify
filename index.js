2b1290e94639bd35f24eae6028b19833f3fa44a1
Max <maxhenson28@icloud.com>
2020-10-17 10:15:22 +0100

    ss

--- index.js
+++ index.js
@@ -102,23 +102,14 @@ bot.on("message", async (message) => {
         }
         }
     } else {
-        if(message.guild.channels.cache.find(c => {
-            var ch = c.name === "secure-verify"
+            var ch = message.guild.channels.cache.find(c => c.name === "secure-verify")
             const error5 = new Discord.MessageEmbed()
-        .setAuthor(message.author.username, message.author.displayAvatarURL())
-        .setDescription("ERROR OCCURED: \n \n ```You can only use this command in the setup verify channel.``` \n Verify channel:")
-        message.channel.send(error5)
-        message.channel.send(`<#${ch.id}>`)
-        })) {
-            message.delete()
-        } else {
-            const error6 = new Discord.MessageEmbed()
             .setAuthor(message.author.username, message.author.displayAvatarURL())
-            .setDescription("ERROR OCCURED: \n \n ```Your server has not been setup with a verify channel.```")
-            message.channel.send(error6)
-            message.delete()
+            .setDescription("ERROR OCCURED: \n \n ```You can only use this command in the setup verify channel.``` \n Verify channel:")
+            message.channel.send(error5)
+            message.channel.send(`<#${ch.id}>`)
         }
-    }}
+    }
     
     if (command == `verifycheck`) {
         if(message.member.roles.cache.find(r => r.name === "Securely Verified")) {
