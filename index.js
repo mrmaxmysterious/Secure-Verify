779e82ba020c6bf3db64ec65ed5f798e0b3e2fb1
Max <maxhenson28@icloud.com>
2020-10-18 12:29:52 +0100

    ss

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ bot.on("guildMemberRemove", member => {
     if(member.guild.id === "767033981828464670") {
         const gud = new Discord.MessageEmbed()
         .setAuthor(member.user.username, member.user.displayAvatarURL({dynamic: true}))
-        .setDescription(member + " Just left our server!")
+        .setDescription(`${member.user.username} just left our server!`)
         member.guild.channels.cache.find(c => c.name === "welcome-x-goodbye").send(member, {embed: gud})
     }
 })
